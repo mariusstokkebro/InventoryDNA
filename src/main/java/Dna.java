f091957935d8c586a5052de4f844dffb8f34cfdb
Frederik <frederikbrieghel@gmail.com>
2020-12-04 12:25:26 +0100

    Halvbegyndt på DNA

--- src/main/java/Dna.java
+++ src/main/java/Dna.java
@@ -1,14 +1,18 @@
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+
 public class Dna {
     PApplet p;
-    ArrayList<Item> dnaString = new ArrayList<Item>;
+    ArrayList<Item> DNAStreng = new ArrayList<Item>();
 
     Dna(PApplet p) {
         this.p = p;
     }
 
     void hentVærdi() {
-        for(int i = 0, )
+        for(int i = 0; i<DNAStreng.size();i++){
+            DNAStreng.add();
+        }
     }
 }
