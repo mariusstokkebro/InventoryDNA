22f542d03d1cfefa10160b4210c974567a54948f
mariuss tokkebro <43348192+mariusstokkebro@users.noreply.github.com>
2020-12-04 12:18:12 +0100

    making item class

---
+++ src/main/java/Item.java
@@ -0,0 +1,11 @@
+public class Item {
+    String name;
+    int weight;
+    int worth;
+
+    Item(String name, int weight, int worth){
+        this.name = name;
+        this.weight = weight;
+        this.worth = worth;
+    }
+}
