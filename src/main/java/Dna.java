42f7e09f6eb6cc6408e662451d8d793b3df02033
mariuss tokkebro <43348192+mariusstokkebro@users.noreply.github.com>
2020-12-04 11:04:05 +0100

    Initial commit

---
+++ src/main/java/Dna.java
@@ -0,0 +1,9 @@
+import processing.core.PApplet;
+
+public class Dna {
+    PApplet p;
+    Dna(PApplet p){
+        this.p = p;
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+    PApplet p;
+
+    @Override
+    public void settings() {
+
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
