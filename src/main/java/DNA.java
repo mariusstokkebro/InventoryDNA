318ae92653599cf527ec02cb505b80e91495a586
Frederik <frederikbrieghel@gmail.com>
2020-12-22 12:35:53 +0100

    Inlists are added

--- src/main/java/DNA.java
+++ src/main/java/DNA.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import processing.data.IntList;
 import processing.data.Table;
 
 import java.util.ArrayList;
@@ -11,6 +12,9 @@ public class DNA {
     ArrayList<Item> DNAString = new ArrayList<Item>();
     ArrayList<Backpack> backpack = new ArrayList<Backpack>();
     List<List<Item>> backpack2 = new ArrayList<List<Item>>();
+    IntList totalWeight = new IntList();
+    IntList totalWorth = new IntList();
+
     Table t;
     int itemInBackpack = 0;
 
@@ -29,23 +33,28 @@ public class DNA {
     void backpack(){
         for(int i = 0; i<50;i++){
             List<Item> list = new ArrayList<>();
-            int totalWeight=0;
-            int totalWorth=0;
+            int weight=0;
+            int worth=0;
             for(int s = 0; s<DNAString.size();s++){
              itemInBackpack = (int) p.random(0,2);
 
                 if(itemInBackpack == 1) {
-
                     list.add(new Item(DNAString.get(s).name, DNAString.get(s).weight, DNAString.get(s).worth));
+                    weight+=DNAString.get(s).weight;
+                    worth+=DNAString.get(s).worth;
                 }
                 else{
 
                 }
             }
+            this.totalWeight.append(weight);
+            this.totalWorth.append(worth);
             backpack2.add(list);
 
         }
        p.println(backpack2.get(2));
+        p.println(totalWeight);
+        p.println(totalWorth);
     }
 
     /*int getTotalWeight(ArrayList a){
