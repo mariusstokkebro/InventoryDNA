1be07f4f6c76fcd46dc0f8e801d5c3c6dda8680b
Frederik <frederikbrieghel@gmail.com>
2020-12-22 16:09:33 +0100

    Mutate funktion is added

--- src/main/java/DNA.java
+++ src/main/java/DNA.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static javax.swing.UIManager.get;
+import static org.apache.commons.lang.RandomStringUtils.random;
 
 public class DNA {
     PApplet p;
@@ -23,29 +24,28 @@ public class DNA {
 
     DNA(PApplet p, Table t) {
         this.p = p;
-        this.t=t;
+        this.t = t;
     }
 
     void assignValue() {
-        for(int i = 1; i<t.getRowCount();i++){
-            DNAString.add(new Item(t.getRow(i).getString(0),t.getRow(i).getInt(1),t.getRow(i).getInt(2)));
+        for (int i = 1; i < t.getRowCount(); i++) {
+            DNAString.add(new Item(t.getRow(i).getString(0), t.getRow(i).getInt(1), t.getRow(i).getInt(2)));
         }
     }
 
-  public void backpack(){
-        for(int i = 0; i<50;i++){
+    public void backpack() {
+        for (int i = 0; i < 50; i++) {
             List<Item> list = new ArrayList<>();
-            int weight=0;
-            int worth=0;
-            for(int s = 0; s<DNAString.size();s++){
-             itemInBackpack = (int) p.random(0,2);
+            int weight = 0;
+            int worth = 0;
+            for (int s = 0; s < DNAString.size(); s++) {
+                itemInBackpack = (int) p.random(0, 2);
 
-                if(itemInBackpack == 1) {
+                if (itemInBackpack == 1) {
                     list.add(new Item(DNAString.get(s).name, DNAString.get(s).weight, DNAString.get(s).worth));
-                    weight+=DNAString.get(s).weight;
-                    worth+=DNAString.get(s).worth;
-                }
-                else{
+                    weight += DNAString.get(s).weight;
+                    worth += DNAString.get(s).worth;
+                } else {
 
                 }
             }
@@ -54,54 +54,64 @@ public class DNA {
             backpack.add(list);
 
         }
-       p.println(backpack.get(2));
-        p.println(totalWeight);
-        p.println(totalWorth);
+        PApplet.println(backpack.get(2));
+        PApplet.println(totalWeight);
+        PApplet.println(totalWorth);
 
     }
-void fitness(){
-    float fitness =0;
-    float totalFitnessValue = 0;
-    for(int i = 0; i<totalWeight.size(); i++){
-        fitness = 0;
-        if(totalWeight.get(i)>=5000){
+
+    void fitness() {
+        float fitness = 0;
+        float totalFitnessValue = 0;
+        for (int i = 0; i < totalWeight.size(); i++) {
             fitness = 0;
-        }
-        else {
-            fitness += totalWeight.get(i);
-            fitness += totalWorth.get(i);
-
-        }
-        totalFitness.append(fitness);
-    }
-    if(totalFitness.size()==totalWeight.size()){
-        for(int i=0;i<totalFitness.size();i++){
-            totalFitnessValue+=totalFitness.get(i);
-        }
-        for(int i=0;i<totalFitness.size();i++){
-            totalFitnessPercentage.append(totalFitness.get(i)/totalFitnessValue*100);
-            p.println(totalFitnessPercentage);
-        }
-
-
-    }
-
-}
-
-DNA crossover(DNA partner){
-DNA child  = new DNA(p, t);
-    int midpoint = (int)(p.random(0,2));
-    for (int i = 0; i < backpack.size(); i++) {
-        for(int s = 0; s<backpack.get(i).size();s++){
-            if (midpoint == 0){ child.backpack.get(i).set(s,backpack.get(i).get(s));
+            if (totalWeight.get(i) >= 5000) {
+                fitness = 0;
+            } else {
+                fitness += totalWeight.get(i);
+                fitness += totalWorth.get(i);
 
             }
-            else child.backpack.get(i).set(s,partner.backpack.get(i).get(s));
+            totalFitness.append(fitness);
+        }
+        if (totalFitness.size() == totalWeight.size()) {
+            for (int i = 0; i < totalFitness.size(); i++) {
+                totalFitnessValue += totalFitness.get(i);
+            }
+            for (int i = 0; i < totalFitness.size(); i++) {
+                totalFitnessPercentage.append(totalFitness.get(i) / totalFitnessValue * 100);
+                PApplet.println(totalFitnessPercentage);
+            }
+
+
         }
 
     }
 
-    return child;
-}
+    DNA crossover(DNA partner) {
+        DNA child = new DNA(this.p, t);
+        int midpoint = (int) (p.random(0, 2));
+        for (int i = 0; i < backpack.size(); i++) {
+            for (int s = 0; s < backpack.get(i).size(); s++) {
+                if (midpoint == 0) {
+                    child.backpack.get(i).set(s, backpack.get(i).get(s));
 
+                } else child.backpack.get(i).set(s, partner.backpack.get(i).get(s));
+            }
+
+        }
+
+        return child;
+    }
+
+
+    void mutate() {
+        float mutationRate = (float) 0.01;
+
+        for (int i = 0; i < backpack.get(i).size(); i++) {
+            if ((float) p.random(1) < mutationRate) {
+                backpack.get(i).set(i,DNAString.get((int) p.random(0,DNAString.size())));
+            }
+        }
+    }
 }
